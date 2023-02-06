c2f21ca5534167f6563b3e884eb6bddb36d77187
baggatbt <baggatbt@gmail.com>
2023-02-06 13:33:27 -0700

    Add/Update/Delete now work, need to implement appointments next

--- src/sample/CustomerController.java
+++ src/sample/CustomerController.java
@@ -1,5 +1,6 @@
 package sample;
 
+import java.io.IOException;
 import java.net.URL;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
@@ -8,11 +9,17 @@ import java.sql.SQLException;
 import java.util.ResourceBundle;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.scene.control.Button;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.stage.Stage;
 
 
 public class CustomerController implements Initializable {
@@ -58,8 +65,6 @@ public class CustomerController implements Initializable {
         // Populate the customer table with data from the database
         populateCustomerTable();
     }
-
-
 
 
     // Method to add a new customer to the database
@@ -154,8 +159,6 @@ public class CustomerController implements Initializable {
             populateCustomerTable();
 
 
-
-
             showDeleteMessage();
         } catch (SQLException e) {
             e.printStackTrace();
@@ -231,10 +234,6 @@ public class CustomerController implements Initializable {
     }
 
 
-
-
-
-
     private void populateCustomerTable() {
         try {
             // Prepare the select statement
@@ -271,5 +270,13 @@ public class CustomerController implements Initializable {
     }
 
 
-
+    public void goToAppointments(ActionEvent actionEvent) throws IOException {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("appointmentsForm.fxml"));
+        Parent root = loader.load();
+        Scene scene = new Scene(root);
+        Stage stage = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
+        stage.setScene(scene);
+        stage.show();
+    }
 }
+
