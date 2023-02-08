846f10160c75831099fa8f96f1f085f9f6398b39
baggatbt <baggatbt@gmail.com>
2023-02-08 09:13:32 -0700

    Appt table loads appt's

--- src/sample/AppointmentsController.java
+++ src/sample/AppointmentsController.java
@@ -61,7 +61,10 @@ public class AppointmentsController implements Initializable {
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
         appointmentIdTextField.setEditable(false);
+        populateTypeComboBox();
+        populateContactComboBox();
         retrieveAppointmentsFromDB();
+
     }
 
     private void retrieveAppointmentsFromDB() {
@@ -104,6 +107,103 @@ public class AppointmentsController implements Initializable {
         } catch (SQLException e) {
             e.printStackTrace();
         }
+    }
+
+    private void populateTypeComboBox() {
+        try {
+
+
+            // Prepare a statement to select all the countries from the database
+            PreparedStatement stmt = JDBC.getConnection().prepareStatement("SELECT Type FROM appointments");
+            // Execute the query and store the results in a ResultSet
+            ResultSet rs = stmt.executeQuery();
+            // Iterate through the results and add each country to the combo box
+            while (rs.next()) {
+                typeComboBox.getItems().add(rs.getString("Type"));
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void populateContactComboBox() {
+        try {
+
+
+            // Prepare a statement to select all the countries from the database
+            PreparedStatement stmt = JDBC.getConnection().prepareStatement("SELECT Contact_Name FROM contacts");
+            // Execute the query and store the results in a ResultSet
+            ResultSet rs = stmt.executeQuery();
+            // Iterate through the results and add each country to the combo box
+            while (rs.next()) {
+                contactComboBox.getItems().add(rs.getString("Contact_Name"));
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @FXML
+    private void addAppointment() {
+        try {
+            Connection connection = JDBC.getConnection();
+
+            Object selectedItem = typeComboBox.getSelectionModel().getSelectedItem();
+            PreparedStatement statement = connection.prepareStatement("SELECT Type FROM appointments WHERE Type = ?");
+            statement.setString(1, (String) selectedItem);
+            ResultSet result = statement.executeQuery();
+            String type = result.getString("Type");
+
+            Object selectedItem2 = contactComboBox.getSelectionModel().getSelectedItem();
+            PreparedStatement statement2 = connection.prepareStatement("SELECT Contact_Name FROM contacts WHERE Contact_Name = ?");
+            statement.setString(1, (String) selectedItem);
+            ResultSet result2 = statement.executeQuery();
+            String contactName = result.getString("Contact");
+
+            java.sql.Date sqlStartDate = java.sql.Date.valueOf(String.valueOf(startDatePicker));
+            java.sql.Date sqlEndDate = java.sql.Date.valueOf(String.valueOf(endDatePicker));
+
+
+            // Prepare the insert statement
+            PreparedStatement stmt = JDBC.getConnection().prepareStatement("INSERT INTO appointments(Title, Description, Location, Type, Start, End, Customer_ID, User_ID) VALUES (?, ?, ?, ?, ?, ?, ?, ?)");
+
+            // Set the values for each column in the table
+            stmt.setString(1, titleTextField.getText());
+            stmt.setString(2, descriptionTextField.getText());
+            stmt.setString(3, locationTextField.getText());
+            stmt.setString(4, type);
+            stmt.setDate(5, sqlStartDate);
+            stmt.setDate(6, sqlEndDate);
+            stmt.setString(7, customerIdTextField.getText());
+            stmt.setString( 8, userIdTextField.getText());
+
+            // Execute the insert statement
+            stmt.executeUpdate();
+
+            clearForm();
+
+
+            // Refresh the table
+            retrieveAppointmentsFromDB();
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+
+        }
+    private void clearForm() {
+        titleTextField.clear();
+        descriptionTextField.clear();
+        locationTextField.clear();
+        typeComboBox.getSelectionModel().clearSelection();
+        contactComboBox.getSelectionModel().clearSelection();
+        customerIdTextField.clear();
+        userIdTextField.clear();
+        startDatePicker.setValue(null);
+        endDatePicker.setValue(null);
     }
 
 
