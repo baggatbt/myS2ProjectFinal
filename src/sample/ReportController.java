d1d7e44fbb209a97a60a3823dcff169abcb21882
baggatbt <baggatbt@gmail.com>
2023-02-28 06:44:49 -0700

    report form created and set up

--- src/sample/ReportController.java
+++ src/sample/ReportController.java
@@ -1,7 +1,14 @@
 package sample;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -13,4 +20,12 @@ public class ReportController implements Initializable {
 
     }
 
+    public void goToAppointments(ActionEvent actionEvent) throws IOException {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("appointmentsForm.fxml"));
+        Parent root = loader.load();
+        Scene scene = new Scene(root);
+        Stage stage = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
+        stage.setScene(scene);
+        stage.show();
+    }
 }
