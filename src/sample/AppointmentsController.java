140dcfdae472e10a8c25812ca207a8f5ca7dcc93
baggatbt <baggatbt@gmail.com>
2023-02-28 06:35:15 -0700

    report form created and set up

--- src/sample/AppointmentsController.java
+++ src/sample/AppointmentsController.java
@@ -1,5 +1,6 @@
 package sample;
 
+import java.io.IOException;
 import java.net.URL;
 import java.sql.*;
 import java.text.ParseException;
@@ -15,9 +16,14 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.stage.Stage;
 
 
 public class AppointmentsController implements Initializable {
@@ -84,8 +90,8 @@ public class AppointmentsController implements Initializable {
     private TextField customerIdTextField;
     @FXML
     private TextField userIdTextField;
-    @FXML
-    private TextArea reportTextArea;
+
+
 
     private ObservableList<Appointment> appointmentList = FXCollections.observableArrayList();
 
@@ -586,6 +592,15 @@ public class AppointmentsController implements Initializable {
     }
 
 
+    public void goToReports(ActionEvent actionEvent) throws IOException {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("reportsForm.fxml"));
+            Parent root = loader.load();
+            Scene scene = new Scene(root);
+            Stage stage = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
+            stage.setScene(scene);
+            stage.show();
+        }
 
-}
+    }
+
 
